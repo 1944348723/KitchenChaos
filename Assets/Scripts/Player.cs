3dfd57b12b0f76d9b6f610dfabf594459ea946ab
zcj <1944348723@qq.com>
2026-01-22 19:02:43 +0800

    导入美术资源，创建地板和玩家，实现玩家移动

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private void Update() {
+        Vector2 inputVector = new(0, 0);
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.y += 1;
+        } else if (Input.GetKey(KeyCode.S)) {
+            inputVector.y -= 1;
+        } else if (Input.GetKey(KeyCode.A)) {
+            inputVector.x -= 1;
+        } else if (Input.GetKey(KeyCode.D)) {
+            inputVector.x += 1;
+        }
+        inputVector.Normalize();
+
+        Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
+        this.transform.position += moveDir * Time.deltaTime;
+    }
+}
