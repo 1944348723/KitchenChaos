2d44bd2b892b814418aeeb4e8056c59b7d48c3ce
zcj <1944348723@qq.com>
2026-01-23 15:41:55 +0800

    实现了碰撞检测

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,18 +7,47 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 10f;
 
+    private const float PLAYER_RADIUS = 0.7f;
+    private const float PLAYER_HEIGHT = 1;
     private bool isWalking = false;
 
     private void Update() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-        // 移动
         Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
-        this.transform.position += moveDir * Time.deltaTime * moveSpeed;
         this.isWalking = !moveDir.Equals(Vector3.zero);
 
         // 转向
         this.transform.forward = Vector3.Slerp(this.transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+        float movement = Time.deltaTime * moveSpeed;
+        Vector3 capsuleBottom = transform.position;
+        Vector3 capsuleTop = transform.position + Vector3.up * PLAYER_HEIGHT;
+
+        // 朝向方向
+        bool canMove = !Physics.CapsuleCast(capsuleBottom, capsuleTop, PLAYER_RADIUS, moveDir, movement);
+        
+        // X方向
+        if (!canMove && moveDir.x != 0f) {
+            Vector3 moveDirX = new(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(capsuleBottom, capsuleTop, PLAYER_RADIUS, moveDirX, movement * Math.Abs(moveDir.x));
+            if (canMove) {
+                moveDir = moveDirX;
+            }
+        }
+
+        // Z方向
+        if (!canMove && moveDir.z != 0f) {
+            Vector3 moveDirZ = new(0, 0, moveDir.z);
+            canMove = !Physics.CapsuleCast(capsuleBottom, capsuleTop, PLAYER_RADIUS, moveDirZ, movement * Math.Abs(moveDir.z));
+            if (canMove) {
+                moveDir = moveDirZ;
+            }
+        }
+
+        if (canMove) {
+            this.transform.position += moveDir * movement;
+        } 
     }
 
     public bool IsWalking() {
