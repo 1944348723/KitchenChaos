51f644f23d95b19c325844b12a841f92c8d91916
zcj <1944348723@qq.com>
2026-01-23 11:43:29 +0800

    重构，将输入系统从Player中抽取出来

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,29 +1,15 @@
 using UnityEngine;
-using UnityEngine.XR;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 10f;
 
     private bool isWalking = false;
 
     private void Update() {
-        // 输入
-        Vector2 inputVector = new(0, 0);
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x += 1;
-        }
-        inputVector.Normalize();
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         // 移动
         Vector3 moveDir = new(inputVector.x, 0, inputVector.y);
