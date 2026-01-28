861e96ac99d253a0d921e247f7b9a2eeb10bc71b
zcj <1944348723@qq.com>
2026-01-28 15:22:07 +0800

    添加了StoveCounter，暂未实现功能

---
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class StoveCounter : MonoBehaviour, IInteractable {
+    public void Interact(Player player) {
+        
+    }
+}
---
+++ Assets/Scripts/Counters/StoveCounterVisual.cs
@@ -0,0 +1,5 @@
+public class StoveCounterVisual : CounterVisual {
+    private new void Start() {
+        base.Start();
+    }
+}
